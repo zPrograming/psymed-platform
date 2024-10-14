49878cffa92c515acc4c2dfcdc0e3d534d7f2f29
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-14 12:13:25 -0500

    chore: initial commit

---
+++ src/test/java/com/closedsource/psymed/platform/PsymedPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.closedsource.psymed.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsymedPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
