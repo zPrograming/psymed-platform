dfeca361d231490d190cf29d260ef05f8c053fc1
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-14 18:16:22 -0500

    feat(patient-report): biological function status record added

---
+++ src/main/java/com/closedsource/psymed/platform/patientreport/domain/model/valueobjects/BiologicalFunctionStatus.java
@@ -0,0 +1,19 @@
+package com.closedsource.psymed.platform.patientreport.domain.model.valueobjects;
+
+public record BiologicalFunctionStatus(Integer hunger, Integer hydration,
+                                       Integer sleep, Integer energy) {
+    public BiologicalFunctionStatus {
+        if (hunger < 0 || hunger >5)
+            throw new IllegalArgumentException("Hunger must be between 0 and 5");
+        if (hydration < 0 || hydration >5)
+            throw new IllegalArgumentException("Hydration must be between 0 and 5");
+        if(sleep <0 || sleep >5)
+            throw new IllegalArgumentException("Sleep must be between 0 and 5");
+        if(energy < 0 || energy >5)
+            throw new IllegalArgumentException("Energy must be between 0 and 5");
+    }
+//    public BiologicalFunctionStatus() {
+//        this(0, 0, 0, 0);
+//    }
+
+}
