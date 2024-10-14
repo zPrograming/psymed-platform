a4c8af53c3ff7c20b4a87a819cd321f1b649c375
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-14 18:16:46 -0500

    feat(patient-report): entities added

---
+++ src/main/java/com/closedsource/psymed/platform/patientreport/domain/model/entities/BiologicalFunction.java
@@ -0,0 +1,28 @@
+package com.closedsource.psymed.platform.patientreport.domain.model.entities;
+
+import com.closedsource.psymed.platform.patientreport.domain.model.valueobjects.BiologicalFunctionStatus;
+import com.closedsource.psymed.platform.shared.domain.model.entities.AuditableModel;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+public class BiologicalFunction extends AuditableModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    BiologicalFunctionStatus status;
+
+    @NotNull
+    private Long patientId;
+
+
+    public BiologicalFunction(Integer hunger, Integer hydration,
+                              Integer sleep, Integer energy, Long patientId) {
+        this.status = new BiologicalFunctionStatus(hunger, hydration, sleep, energy);
+        this.patientId = patientId;
+    }
+
+}
