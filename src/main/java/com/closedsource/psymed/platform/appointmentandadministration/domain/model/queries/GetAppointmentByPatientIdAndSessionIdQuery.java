11c70313562b00515288673d05eb8eb76a134c00
Anatoly69420 <u202211813@upc.edu.pe>
2024-10-17 13:04:00 -0500

    feat(appointment-and-administration): add query to retrieve an appointment by patient ID and appointment ID

---
+++ src/main/java/com/closedsource/psymed/platform/appointmentandadministration/domain/model/queries/GetAppointmentByPatientIdAndSessionIdQuery.java
@@ -0,0 +1,17 @@
+package com.closedsource.psymed.platform.appointmentandadministration.domain.model.queries;
+
+/**
+ * Query to get a specific appointment for a patient by appointment ID.
+ * @param patientId The ID of the patient.
+ * @param appointmentId The ID of the appointment.
+ */
+public record GetAppointmentByPatientIdAndSessionIdQuery(Long patientId, Long appointmentId) {
+    public GetAppointmentByPatientIdAndSessionIdQuery {
+        if (patientId == null || patientId <= 0) {
+            throw new IllegalArgumentException("patientId cannot be null or less than or equal to 0");
+        }
+        if (appointmentId == null || appointmentId <= 0) {
+            throw new IllegalArgumentException("appointmentId cannot be null or less than or equal to 0");
+        }
+    }
+}
