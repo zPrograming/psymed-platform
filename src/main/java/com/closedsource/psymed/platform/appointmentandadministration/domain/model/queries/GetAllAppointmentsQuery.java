a4ae1a4155941b6bef100ff03a384e9a47c7d483
Anatoly69420 <u202211813@upc.edu.pe>
2024-10-17 13:03:45 -0500

    feat(appointment-and-administration): add query to retrieve all appointments

---
+++ src/main/java/com/closedsource/psymed/platform/appointmentandadministration/domain/model/queries/GetAllAppointmentsQuery.java
@@ -0,0 +1,6 @@
+package com.closedsource.psymed.platform.appointmentandadministration.domain.model.queries;
+
+/**
+ * Query to get all appointments in the system.
+ */
+public record GetAllAppointmentsQuery() {}
