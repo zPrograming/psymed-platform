8fced1989c7bc84945f054399c0c5b3121347ddd
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-18 22:09:58 -0500

    feat(patient-report): creating commands added

---
+++ src/main/java/com/closedsource/psymed/platform/patientreport/domain/model/commands/CreateBiologicalFunctionRecordCommand.java
@@ -0,0 +1,4 @@
+package com.closedsource.psymed.platform.patientreport.domain.model.commands;
+
+public record CreateBiologicalFunctionRecordCommand() {
+}
---
+++ src/main/java/com/closedsource/psymed/platform/patientreport/domain/model/commands/CreateMoodStateRecordCommand.java
@@ -0,0 +1,4 @@
+package com.closedsource.psymed.platform.patientreport.domain.model.commands;
+
+public record CreateMoodStateRecordCommand(Long patientId, Integer moodStatus) {
+}
