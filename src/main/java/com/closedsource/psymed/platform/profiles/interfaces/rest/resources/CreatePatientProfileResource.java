4f27c889f5807de25fd11e29447457c07583e47e
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-21 17:21:15 -0500

    feat(profiles): interface resources added

---
+++ src/main/java/com/closedsource/psymed/platform/profiles/interfaces/rest/resources/CreatePatientProfileResource.java
@@ -0,0 +1,9 @@
+package com.closedsource.psymed.platform.profiles.interfaces.rest.resources;
+
+public record CreatePatientProfileResource(    String firstName,
+                                               String lastName,
+                                               String street,
+                                               String city,
+                                               String country,
+                                               String email
+                                               ) { }
