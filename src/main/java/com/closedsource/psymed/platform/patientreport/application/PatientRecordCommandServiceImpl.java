40a2034161977eafcbc69e77e15799edfa8058b3
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-21 00:39:19 -0500

    feat(patient-report): patient record command service added without external profile service

--- src/main/java/com/closedsource/psymed/platform/patientreport/application/PatientRecordCommandServiceImpl.java
+++ src/main/java/com/closedsource/psymed/platform/patientreport/application/PatientRecordCommandServiceImpl.java
@@ -1,11 +1,31 @@
-package com.closedsource.psymed.platform.patientreport.application;
+    package com.closedsource.psymed.platform.patientreport.application;
 
-import com.closedsource.psymed.platform.patientreport.domain.model.commands.CreateMoodStateRecordCommand;
-import com.closedsource.psymed.platform.patientreport.services.PatientRecordCommandService;
+    import com.closedsource.psymed.platform.patientreport.domain.model.aggregates.MoodStateRecord;
+    import com.closedsource.psymed.platform.patientreport.domain.model.commands.CreateMoodStateRecordCommand;
+    import com.closedsource.psymed.platform.patientreport.infrastructure.persistence.jpa.repositories.MoodStateRecordRepository;
+    import com.closedsource.psymed.platform.patientreport.services.PatientRecordCommandService;
 
-public class PatientRecordCommandServiceImpl implements PatientRecordCommandService {
-    @Override
-    public void recordMoodState(CreateMoodStateRecordCommand command) throws IllegalStateException {
+    public class PatientRecordCommandServiceImpl implements PatientRecordCommandService {
+        private final MoodStateRecordRepository moodStateRecordRepository;
 
+        public PatientRecordCommandServiceImpl(MoodStateRecordRepository moodStateRepository) {
+            this.moodStateRecordRepository = moodStateRepository;
+        }
+        @Override
+        public void recordMoodState(CreateMoodStateRecordCommand command) {
+//            #TODO: Implement this method to fetch if the patient exists
+//            var patientId = externalProfileService.existsById(command.patientId());
+//            if(patientId) {
+//                var moodStateRecord = new MoodStateRecord(command.patientId(), command.moodStatus());
+//                var lastMoodStateRecord = moodStateRecordRepository.findAllByPatientId(moodStateRecord.getPatientId()).stream().reduce((first, second) -> second).orElse(null);
+//                moodStateRecord.validateDay(lastMoodStateRecord);
+//                moodStateRecordRepository.save(moodStateRecord);
+//            }
+//            else {
+//                throw new IllegalArgumentException("Patient does not exist");
+//            }
+
+
+
+        }
     }
-}
