e41f0a370aa9b946b3adab496592652b99a128c7
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-21 17:54:26 -0500

    feat(profiles): service updated to handle the new commands

--- src/main/java/com/closedsource/psymed/platform/profiles/domain/services/PatientProfileCommandService.java
+++ src/main/java/com/closedsource/psymed/platform/profiles/domain/services/PatientProfileCommandService.java
@@ -1,10 +1,12 @@
 package com.closedsource.psymed.platform.profiles.domain.services;
 
 import com.closedsource.psymed.platform.profiles.domain.model.aggregates.PatientProfile;
+import com.closedsource.psymed.platform.profiles.domain.model.commands.CheckPatientProfileByIdCommand;
 import com.closedsource.psymed.platform.profiles.domain.model.commands.CreatePatientProfileCommand;
 
 import java.util.Optional;
 
 public interface PatientProfileCommandService {
     Optional<PatientProfile> handle(CreatePatientProfileCommand command);
+    boolean handle(CheckPatientProfileByIdCommand command);
 }
