41f9f9229453424b0f02b2c83db72008a2d97688
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-21 00:38:25 -0500

    feat(patient-report): repository added in infrastructure layer

---
+++ src/main/java/com/closedsource/psymed/platform/patientreport/infrastructure/persistence/jpa/repositories/BiologicalFunctionRecordRepository.java
@@ -0,0 +1,6 @@
+package com.closedsource.psymed.platform.patientreport.infrastructure.persistence.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BiologicalFunctionRecordRepository {
+}
