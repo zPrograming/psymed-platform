741306f226a85fe72b9a5e41340a84123da70498
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-21 00:40:05 -0500

    feat(patient-report): patient record command service added

--- src/main/java/com/closedsource/psymed/platform/patientreport/services/PatientRecordCommandService.java
+++ src/main/java/com/closedsource/psymed/platform/patientreport/services/PatientRecordCommandService.java
@@ -3,6 +3,6 @@ package com.closedsource.psymed.platform.patientreport.services;
 import com.closedsource.psymed.platform.patientreport.domain.model.commands.CreateMoodStateRecordCommand;
 
 public interface PatientRecordCommandService {
-    void recordMoodState(CreateMoodStateRecordCommand command) throws IllegalStateException;
+    void recordMoodState(CreateMoodStateRecordCommand command);
 
 }
