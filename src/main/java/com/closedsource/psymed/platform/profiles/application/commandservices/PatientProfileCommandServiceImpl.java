57a752c09e1b8251eaf3d9463f09fd5ffcd3c935
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-21 17:19:43 -0500

    feat(profiles): command services in application layer added

---
+++ src/main/java/com/closedsource/psymed/platform/profiles/application/commandservices/PatientProfileCommandServiceImpl.java
@@ -0,0 +1,37 @@
+package com.closedsource.psymed.platform.profiles.application.commandservices;
+
+import com.closedsource.psymed.platform.profiles.domain.model.aggregates.PatientProfile;
+import com.closedsource.psymed.platform.profiles.domain.model.commands.CreatePatientProfileCommand;
+import com.closedsource.psymed.platform.profiles.domain.model.valueobjects.Email;
+import com.closedsource.psymed.platform.profiles.domain.services.PatientProfileCommandService;
+import com.closedsource.psymed.platform.profiles.infrastructure.persistence.jpa.repositories.PatientProfileRepository;
+import com.closedsource.psymed.platform.profiles.infrastructure.persistence.jpa.repositories.ProfessionalProfileRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PatientProfileCommandServiceImpl implements PatientProfileCommandService {
+
+    private final PatientProfileRepository patientProfileRepository;
+    private final ProfessionalProfileRepository professionalProfileRepository;
+
+    public PatientProfileCommandServiceImpl(PatientProfileRepository patientProfileRepository, ProfessionalProfileRepository professionalProfileRepository) {
+        this.patientProfileRepository = patientProfileRepository;
+        this.professionalProfileRepository = professionalProfileRepository;
+    }
+
+    @Override
+    public Optional<PatientProfile> handle(CreatePatientProfileCommand command) {
+        var emailAddress = new Email(command.email());
+
+        if(patientProfileRepository.existsByEmail(emailAddress) || professionalProfileRepository.existsByEmail(emailAddress))
+            throw new IllegalArgumentException("Email already exists");
+
+        var patientProfile = new PatientProfile(command);
+        patientProfileRepository.save(patientProfile);
+
+        return Optional.of(patientProfile);
+    }
+}
