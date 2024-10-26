9c814cfcedba692415fc01572d72d6d027f1f624
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-26 16:31:44 -0500

    feat(patient-report): biological function create logic added to service implementation

--- src/main/java/com/closedsource/psymed/platform/patientreport/application/commandservices/BiologicalFunctionCommandServiceImpl.java
+++ src/main/java/com/closedsource/psymed/platform/patientreport/application/commandservices/BiologicalFunctionCommandServiceImpl.java
@@ -1,4 +1,48 @@
 package com.closedsource.psymed.platform.patientreport.application.commandservices;
 
-public class BiologicalFunctionCommandServiceImpl {
+import com.closedsource.psymed.platform.patientreport.application.outboundservices.acl.ExternalProfileService;
+import com.closedsource.psymed.platform.patientreport.domain.exceptions.PatientNotFoundException;
+import com.closedsource.psymed.platform.patientreport.domain.model.aggregates.BiologicalFunction;
+import com.closedsource.psymed.platform.patientreport.domain.model.commands.CreateBiologicalFunctionRecordCommand;
+import com.closedsource.psymed.platform.patientreport.domain.services.BiologicalFunctionCommandService;
+import com.closedsource.psymed.platform.patientreport.infrastructure.persistence.jpa.repositories.BiologicalFunctionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BiologicalFunctionCommandServiceImpl implements BiologicalFunctionCommandService {
+    private final BiologicalFunctionRepository biologicalFunctionRepository;
+    private final ExternalProfileService externalProfileService;
+
+    public BiologicalFunctionCommandServiceImpl(BiologicalFunctionRepository biologicalFunctionRepository,
+                                                ExternalProfileService externalProfileService){
+        this.biologicalFunctionRepository = biologicalFunctionRepository;
+        this.externalProfileService = externalProfileService;
+
+    }
+
+    @Override
+    public Optional<BiologicalFunction> handle(CreateBiologicalFunctionRecordCommand command) {
+        var patientIdExists = externalProfileService.existsPatientById(command.patientId());
+
+        if(patientIdExists) {
+            var biologicalFunctionRecord = new BiologicalFunction(command);
+            var lastBiologicalFunctionRecord = biologicalFunctionRepository
+                    .findAllByPatientId(biologicalFunctionRecord.getPatientId())
+                    .stream().reduce((first, second) -> second).orElse(null);
+
+            if(lastBiologicalFunctionRecord != null)
+                biologicalFunctionRecord.validateRecordAvailability(lastBiologicalFunctionRecord);
+
+            try {
+                var createdBiologicalFunctionRecord = biologicalFunctionRepository.save(biologicalFunctionRecord);
+                return Optional.of(createdBiologicalFunctionRecord);
+            }catch(Exception e){
+                throw new IllegalArgumentException("Error creating the biological function record: %s"
+                        .formatted(e.getMessage()));
+            }
+        }
+        throw new PatientNotFoundException(command.patientId());
+    }
 }
