c865af4b9bb726d79c20b62c3d471534b1ac2ab3
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-26 14:11:26 -0500

    fix(patient-report): persistence jpa repository added for mood state

--- src/main/java/com/closedsource/psymed/platform/patientreport/infrastructure/persistence/jpa/repositories/MoodStateRecordRepository.java
+++ src/main/java/com/closedsource/psymed/platform/patientreport/infrastructure/persistence/jpa/repositories/MoodStateRecordRepository.java
@@ -1,13 +1,12 @@
 package com.closedsource.psymed.platform.patientreport.infrastructure.persistence.jpa.repositories;
 
-import com.closedsource.psymed.platform.patientreport.domain.model.aggregates.BiologicalFunctionRecord;
-import com.closedsource.psymed.platform.patientreport.domain.model.aggregates.MoodStateRecord;
+import com.closedsource.psymed.platform.patientreport.domain.model.aggregates.MoodState;
 import com.closedsource.psymed.platform.patientreport.domain.model.valueobjects.PatientId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MoodStateRecordRepository extends JpaRepository<MoodStateRecord, Long> {
-    List<MoodStateRecord> findAllByPatientId(PatientId patientId);
+public interface MoodStateRecordRepository extends JpaRepository<MoodState, Long> {
+    List<MoodState> findAllByPatientId(PatientId patientId);
     boolean existsByPatientId(PatientId patientId);
 }
