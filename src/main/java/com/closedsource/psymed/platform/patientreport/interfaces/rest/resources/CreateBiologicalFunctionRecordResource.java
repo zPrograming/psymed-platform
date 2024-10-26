ab744a0a15e143ea00e7d63d508874d968a78bcb
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-26 14:09:40 -0500

    fix(patient-report): create and response resources added

---
+++ src/main/java/com/closedsource/psymed/platform/patientreport/interfaces/rest/resources/CreateBiologicalFunctionRecordResource.java
@@ -0,0 +1,6 @@
+package com.closedsource.psymed.platform.patientreport.interfaces.rest.resources;
+
+public record CreateBiologicalFunctionRecordResource(Integer hunger,
+                                                     Integer hydration, Integer sleep,
+                                                     Integer energy, Long patientId) {
+}
