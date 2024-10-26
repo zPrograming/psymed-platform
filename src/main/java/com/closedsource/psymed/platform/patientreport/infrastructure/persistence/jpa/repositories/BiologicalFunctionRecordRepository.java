1f21123fac79582e747bcf524a4d4ad02687f91b
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-26 16:28:35 -0500

    feat(patient-report): biological function repository added

--- src/main/java/com/closedsource/psymed/platform/patientreport/infrastructure/persistence/jpa/repositories/BiologicalFunctionRecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.closedsource.psymed.platform.patientreport.infrastructure.persistence.jpa.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BiologicalFunctionRecordRepository {
-}
