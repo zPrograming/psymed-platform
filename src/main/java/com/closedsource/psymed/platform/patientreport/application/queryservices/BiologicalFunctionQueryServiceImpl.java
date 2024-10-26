4d372ab803623fdba38ab3f3e1fd1c7fd359ba44
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-26 14:17:18 -0500

    feat(patient-report): query services implementation added

---
+++ src/main/java/com/closedsource/psymed/platform/patientreport/application/queryservices/BiologicalFunctionQueryServiceImpl.java
@@ -0,0 +1,7 @@
+package com.closedsource.psymed.platform.patientreport.application.queryservices;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BiologicalFunctionQueryServiceImpl {
+}
---
+++ src/main/java/com/closedsource/psymed/platform/patientreport/application/queryservices/MoodStateQueryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.closedsource.psymed.platform.patientreport.application.queryservices;
+
+import com.closedsource.psymed.platform.patientreport.domain.services.MoodStateQueryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MoodStateQueryServiceImpl extends MoodStateQueryService {
+}
