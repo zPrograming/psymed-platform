635579c42617f1c078ca3e1c08b0b59a9020106e
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-27 17:06:54 -0500

    feat(iam): resources and transform package added in interface layer

---
+++ src/main/java/com/closedsource/psymed/platform/iam/interfaces/rest/resources/SignInResource.java
@@ -0,0 +1,4 @@
+package com.closedsource.psymed.platform.iam.interfaces.rest.resources;
+
+public record SignInResource(String username, String password) {
+}
---
+++ src/main/java/com/closedsource/psymed/platform/iam/interfaces/rest/resources/SignUpResource.java
@@ -0,0 +1,4 @@
+package com.closedsource.psymed.platform.iam.interfaces.rest.resources;
+
+public record SignUpResource(String username, String password, String role) {
+}
