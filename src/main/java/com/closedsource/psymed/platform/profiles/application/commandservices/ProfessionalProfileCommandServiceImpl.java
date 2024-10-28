8b59b9cb430edd3a62e54d777d2e6d3534856eb6
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-27 23:19:24 -0500

    refactor(profiles): professional command service using external service

--- src/main/java/com/closedsource/psymed/platform/profiles/application/commandservices/ProfessionalProfileCommandServiceImpl.java
+++ src/main/java/com/closedsource/psymed/platform/profiles/application/commandservices/ProfessionalProfileCommandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.closedsource.psymed.platform.profiles.application.commandservices;
 
+import com.closedsource.psymed.platform.profiles.application.outboundservices.acl.ExternalAccountService;
 import com.closedsource.psymed.platform.profiles.domain.model.aggregates.ProfessionalProfile;
 import com.closedsource.psymed.platform.profiles.domain.model.commands.CheckProfessionalProfileByIdCommand;
 import com.closedsource.psymed.platform.profiles.domain.model.commands.CreateProfessionalProfileCommand;
@@ -15,20 +16,30 @@ import java.util.Optional;
 @Transactional
 @Service
 public class ProfessionalProfileCommandServiceImpl implements ProfessionalProfileCommandService {
+
     private final ProfessionalProfileRepository professionalProfileRepository;
     private final PatientProfileRepository patientProfileRepository;
-    public ProfessionalProfileCommandServiceImpl(ProfessionalProfileRepository professionalProfileRepository, PatientProfileRepository patientProfileRepository) {
+    private final ExternalAccountService externalAccountService;
+
+    public ProfessionalProfileCommandServiceImpl(ProfessionalProfileRepository professionalProfileRepository,
+                                                 PatientProfileRepository patientProfileRepository,
+                                                 ExternalAccountService externalAccountService) {
         this.professionalProfileRepository = professionalProfileRepository;
         this.patientProfileRepository = patientProfileRepository;
+        this.externalAccountService = externalAccountService;
     }
 
     @Override
+    @Transactional
     public Optional<ProfessionalProfile> handle(CreateProfessionalProfileCommand command) {
         var emailAddress = new Email(command.email());
         if(professionalProfileRepository.existsByEmail(emailAddress) || patientProfileRepository.existsByEmail(emailAddress))
             throw new IllegalArgumentException("Email already exists");
 
-        var profile = new ProfessionalProfile(command);
+        var accountId = externalAccountService.createAccount(command.username(), command.password(), "ROLE_PROFESSIONAL");
+
+        var profile = new ProfessionalProfile(command, accountId.get());
+
         professionalProfileRepository.save(profile);
 
         return Optional.of(profile);
