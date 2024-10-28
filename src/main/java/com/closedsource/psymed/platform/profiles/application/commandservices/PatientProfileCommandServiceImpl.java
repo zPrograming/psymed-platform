182714a8542962bbc3f0471259e1b72376aa1883
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-27 23:05:23 -0500

    feat(profiles): external account service added to the command service

--- src/main/java/com/closedsource/psymed/platform/profiles/application/commandservices/PatientProfileCommandServiceImpl.java
+++ src/main/java/com/closedsource/psymed/platform/profiles/application/commandservices/PatientProfileCommandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.closedsource.psymed.platform.profiles.application.commandservices;
 
+import com.closedsource.psymed.platform.profiles.application.outboundservices.acl.ExternalAccountService;
 import com.closedsource.psymed.platform.profiles.domain.model.aggregates.PatientProfile;
 import com.closedsource.psymed.platform.profiles.domain.model.commands.CheckPatientProfileByIdCommand;
 import com.closedsource.psymed.platform.profiles.domain.model.commands.CreatePatientProfileCommand;
@@ -8,6 +9,7 @@ import com.closedsource.psymed.platform.profiles.domain.services.PatientProfileC
 import com.closedsource.psymed.platform.profiles.infrastructure.persistence.jpa.repositories.PatientProfileRepository;
 import com.closedsource.psymed.platform.profiles.infrastructure.persistence.jpa.repositories.ProfessionalProfileRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -16,20 +18,27 @@ public class PatientProfileCommandServiceImpl implements PatientProfileCommandSe
 
     private final PatientProfileRepository patientProfileRepository;
     private final ProfessionalProfileRepository professionalProfileRepository;
+    private final ExternalAccountService externalAccountService;
 
-    public PatientProfileCommandServiceImpl(PatientProfileRepository patientProfileRepository, ProfessionalProfileRepository professionalProfileRepository) {
+    public PatientProfileCommandServiceImpl(PatientProfileRepository patientProfileRepository,
+                                            ProfessionalProfileRepository professionalProfileRepository,
+                                            ExternalAccountService externalAccountService) {
         this.patientProfileRepository = patientProfileRepository;
         this.professionalProfileRepository = professionalProfileRepository;
+        this.externalAccountService = externalAccountService;
     }
 
+
     @Override
+    @Transactional
     public Optional<PatientProfile> handle(CreatePatientProfileCommand command) {
         var emailAddress = new Email(command.email());
 
         if(patientProfileRepository.existsByEmail(emailAddress) || professionalProfileRepository.existsByEmail(emailAddress))
             throw new IllegalArgumentException("Email already exists");
 
-        var patientProfile = new PatientProfile(command);
+        var accountId = externalAccountService.createAccount(command.username(), command.password(), "ROLE_PATIENT");
+        var patientProfile = new PatientProfile(command, accountId.get());
         patientProfileRepository.save(patientProfile);
 
         return Optional.of(patientProfile);
