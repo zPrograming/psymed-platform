95c9d4ff9e668d38f8d32823d5ab0f4ad3fb16f2
Ro <u202213765@upc.edu.pe>
2024-10-29 10:42:44 -0500

    feat(medication): add professional id value object restrictions.

--- src/main/java/com/closedsource/psymed/platform/treatment/domain/model/valueobjects/ProfessionalId.java
+++ src/main/java/com/closedsource/psymed/platform/treatment/domain/model/valueobjects/ProfessionalId.java
@@ -5,5 +5,9 @@ import jakarta.persistence.Embeddable;
 @Embeddable
 public record ProfessionalId(Long professionalId) {
 
+    public ProfessionalId{
+        if(professionalId == null|| professionalId <=0 )
+            throw new IllegalArgumentException("Professional Id cannot be 0 or less");
+    }
 
 }
