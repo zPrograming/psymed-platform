a9ac928e81ad860a293a816cfbe653ba58ef0f79
RominaMaita <u202213765@upc.edu.pe>
2024-10-28 22:29:15 -0500

    chore(medication): add new value objects.

---
+++ src/main/java/com/closedsource/psymed/platform/treatment/domain/model/valueobjects/PatientId.java
@@ -0,0 +1,10 @@
+package com.closedsource.psymed.platform.treatment.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record PatientId(Long patientId) {
+
+
+
+}
---
+++ src/main/java/com/closedsource/psymed/platform/treatment/domain/model/valueobjects/ProfessionalId.java
@@ -0,0 +1,9 @@
+package com.closedsource.psymed.platform.treatment.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record ProfessionalId(Long professionalId) {
+
+
+}
