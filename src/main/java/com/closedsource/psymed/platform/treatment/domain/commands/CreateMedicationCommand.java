9c4ca182a0a6673266d51895754693f1797254de
RominaMaita <u202213765@upc.edu.pe>
2024-10-28 20:56:50 -0500

    feat(medication): add create medication command.

---
+++ src/main/java/com/closedsource/psymed/platform/treatment/domain/commands/CreateMedicationCommand.java
@@ -0,0 +1,12 @@
+package com.closedsource.psymed.platform.treatment.domain.commands;
+
+public record CreateMedicationCommand(String title, String description) {
+
+    public CreateMedicationCommand{
+        if(title == null || title.isBlank())
+            throw new IllegalArgumentException("Title cannot be null or blank");
+
+        if(description==null || description.isBlank())
+            throw new IllegalArgumentException("Description cannot be null or blank");
+    }
+}
