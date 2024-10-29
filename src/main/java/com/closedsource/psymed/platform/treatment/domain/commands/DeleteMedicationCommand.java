07b0f2cbb2d32ac6602b529629cc1783a5486e5b
RominaMaita <u202213765@upc.edu.pe>
2024-10-28 21:02:12 -0500

    feat(medication): add delete medication command.

---
+++ src/main/java/com/closedsource/psymed/platform/treatment/domain/commands/DeleteMedicationCommand.java
@@ -0,0 +1,8 @@
+package com.closedsource.psymed.platform.treatment.domain.commands;
+
+public record DeleteMedicationCommand(Long medicationId) {
+
+    public DeleteMedicationCommand{
+        if (medicationId ==null || medicationId <=0) throw new IllegalArgumentException("The medication Id cannot be 0 or less");
+    }
+}
