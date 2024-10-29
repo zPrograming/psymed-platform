27cdf64e41587945e1e15a5749066ae046e451ed
Ro <u202213765@upc.edu.pe>
2024-10-29 10:43:50 -0500

    feat(medication): add patient id value object restrictions.

--- src/main/java/com/closedsource/psymed/platform/treatment/domain/model/valueobjects/PatientId.java
+++ src/main/java/com/closedsource/psymed/platform/treatment/domain/model/valueobjects/PatientId.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Embeddable;
 
 @Embeddable
 public record PatientId(Long patientId) {
+    public PatientId{
+        if(patientId == null || patientId <=0 )
+            throw new IllegalArgumentException("Patient id cannot be 0 ore less");
+    }
 
 
 
