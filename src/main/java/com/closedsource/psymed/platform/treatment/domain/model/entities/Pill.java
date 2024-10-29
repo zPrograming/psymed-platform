2fb7d02ab1258fb5548513885046b812dd232711
RominaMaita <u202213765@upc.edu.pe>
2024-10-28 22:29:25 -0500

    chore(medication): add pill logic.

--- src/main/java/com/closedsource/psymed/platform/treatment/domain/model/entities/Pill.java
+++ src/main/java/com/closedsource/psymed/platform/treatment/domain/model/entities/Pill.java
@@ -1,7 +1,6 @@
 package com.closedsource.psymed.platform.treatment.domain.model.entities;
 
 import com.closedsource.psymed.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
-import com.closedsource.psymed.platform.treatment.domain.model.aggregates.Medication;
 import com.closedsource.psymed.platform.treatment.domain.model.valueobjects.PillStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -9,7 +8,7 @@ import lombok.Getter;
 
 @Getter
 @Entity
-public class Pill extends AuditableAbstractAggregateRoot<Pill> {
+public class Pill{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,31 +18,17 @@ public class Pill extends AuditableAbstractAggregateRoot<Pill> {
     private String name;
     @NotNull
     private String description;
-    @NotNull
-    private String frequency;
-    @NotNull
-    private Integer quantity;
 
-    @Enumerated(EnumType.STRING)
-    @NotNull
-    private PillStatus status;
 
-    public Pill(String name, String description, String frequency, Integer quantity, PillStatus status){
+    //@Enumerated(EnumType.STRING)
+    //@NotNull
+    //private PillStatus status;
+
+    public Pill(String name, String description){
         this.name = name;
         this.description = description;
-        this.frequency = frequency;
-        this.quantity = quantity;
-        this.status = status;
-    }
 
-    public Pill(String name, String description, String frequency, Integer quantity){
-        this.name = name;
-        this.description = description;
-        this.frequency = frequency;
-        this.quantity = quantity;
-        this.status = PillStatus.SCHEDULED;
     }
- 
 
     public Pill() {
 
