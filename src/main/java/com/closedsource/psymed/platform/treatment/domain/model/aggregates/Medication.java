0d234d36e222a89f9ca4f1add4b5e6034f561f59
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-10-28 21:23:44 -0500

    feat: add medication function to remove, update and status management

--- src/main/java/com/closedsource/psymed/platform/treatment/domain/model/aggregates/Medication.java
+++ src/main/java/com/closedsource/psymed/platform/treatment/domain/model/aggregates/Medication.java
@@ -4,6 +4,7 @@ package com.closedsource.psymed.platform.treatment.domain.model.aggregates;
 import com.closedsource.psymed.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 import com.closedsource.psymed.platform.treatment.domain.model.entities.Pill;
 import com.closedsource.psymed.platform.treatment.domain.model.valueobjects.MedicalPrescription;
+import com.closedsource.psymed.platform.treatment.domain.model.valueobjects.MedicationStatus;
 import io.jsonwebtoken.lang.Strings;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -16,6 +17,7 @@ public class Medication extends AuditableAbstractAggregateRoot<Medication> {
 
     private String title;
     private String description;
+    private MedicationStatus status;
 
     @Embedded
     private MedicalPrescription medicalPrescription = new MedicalPrescription();
@@ -24,11 +26,27 @@ public class Medication extends AuditableAbstractAggregateRoot<Medication> {
         this.title = title;
         this.description = description;
     }
+    public Medication(String title, String description, MedicationStatus status){
+        this.title = title;
+        this.description = description;
+        this.status = MedicationStatus.ASSIGN;
+    }
+
 
     public Medication(){this(Strings.EMPTY, Strings.EMPTY);}
 
+
     public void addMedication(Pill pill){
         this.medicalPrescription.addPill(pill);
+    }
+
+    public Pill getLastPillInMedicalPrescription() {
+        return this.medicalPrescription.getLastPillInMedicalPrescription();
+    }
+
+
+    public void deleteMedication(Pill pill){
+        this.medicalPrescription.removePill(pill);
     }
 
     public Medication updateMedication(String title, String description){
