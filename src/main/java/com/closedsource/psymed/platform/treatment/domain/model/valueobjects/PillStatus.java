2c052ce264e4067ad1acac5792660bef1984697a
Ro <u202213765@upc.edu.pe>
2024-10-28 19:17:15 -0500

    feat(medication): add pill status.

---
+++ src/main/java/com/closedsource/psymed/platform/treatment/domain/model/valueobjects/PillStatus.java
@@ -0,0 +1,11 @@
+package com.closedsource.psymed.platform.treatment.domain.model.valueobjects;
+
+public enum PillStatus {
+    TAKEN,
+    MISSED,
+    PENDING,
+    SKIPPED,
+    EXPIRED,
+    STOPPED,
+    SCHEDULED,
+}
