a7482481819b10a1ca88f3bc9612bca69b78d09d
Anatoly Noriega <u202211813@upc.edu.pe>
2024-10-28 22:10:54 -0500

    refactor: updated changes in create session command from resource assembler, session from entity assembler and session resource

--- src/main/java/com/closedsource/psymed/platform/appointmentandadministration/interfaces/rest/transform/CreateSessionCommandFromResourceAssembler.java
+++ src/main/java/com/closedsource/psymed/platform/appointmentandadministration/interfaces/rest/transform/CreateSessionCommandFromResourceAssembler.java
@@ -32,8 +32,8 @@ public class CreateSessionCommandFromResourceAssembler {
         Date appointmentDate = parseDate(resource.appointmentDate());
 
         return new CreateSessionCommand(
-                new PatientId(patientId),
-                new ProfessionalId(professionalId),
+                new PatientId(patientId).patientId(),
+                new ProfessionalId(professionalId).professionalId(),
                 new AppointmentDate(appointmentDate),
                 new SessionTime(resource.sessionTime())
         );
