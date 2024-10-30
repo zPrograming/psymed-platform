e572ab872aba60d2a3816a89b83f641d9cd77901
RominaMaita <u202213765@upc.edu.pe>
2024-10-30 12:10:03 -0500

    chore(medication): add aggregate.

--- src/main/java/com/closedsource/psymed/platform/medication/domain/model/aggregates/Medication.java
+++ src/main/java/com/closedsource/psymed/platform/medication/domain/model/aggregates/Medication.java
@@ -1,21 +1,10 @@
 package com.closedsource.psymed.platform.medication.domain.model.aggregates;
 
 import com.closedsource.psymed.platform.medication.domain.model.commands.CreateMedicationCommand;
-import com.closedsource.psymed.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
-import jakarta.persistence.*;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
-@Getter
 @Entity
-//@NoArgsConstructor
 public class Medication {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private String name;
     private String description;
 
@@ -27,10 +16,5 @@ public class Medication {
     public Medication(CreateMedicationCommand command) {
         this.name = command.name();
         this.description = command.description();
-    }
-
-
-    public Medication() {
-
     }
 }
