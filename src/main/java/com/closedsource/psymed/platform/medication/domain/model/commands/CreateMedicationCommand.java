9f8622817e080d3a7217832f9440988d92bed698
RominaMaita <u202213765@upc.edu.pe>
2024-10-30 12:13:12 -0500

    feat: add commands.

--- src/main/java/com/closedsource/psymed/platform/medication/domain/model/commands/CreateMedicationCommand.java
+++ src/main/java/com/closedsource/psymed/platform/medication/domain/model/commands/CreateMedicationCommand.java
@@ -1,11 +1,4 @@
 package com.closedsource.psymed.platform.medication.domain.model.commands;
 
 public record CreateMedicationCommand(String name, String description) {
-
-    public CreateMedicationCommand{
-        if(name == null || name.isBlank())
-            throw new IllegalArgumentException("Medication name cannot be null or empty");
-        if(description == null || description.isBlank())
-            throw new IllegalArgumentException("Medication description cannot be null or empty");
-    }
 }
--- src/main/java/com/closedsource/psymed/platform/medication/domain/model/commands/DeleteMedicationCommand.java
+++ src/main/java/com/closedsource/psymed/platform/medication/domain/model/commands/DeleteMedicationCommand.java
@@ -1,11 +1,3 @@
 package com.closedsource.psymed.platform.medication.domain.model.commands;
 
-public record DeleteMedicationCommand(Long medicationId) {
-
-    public DeleteMedicationCommand{
-        if( medicationId == null ||medicationId <= 0)
-        {
-            throw new IllegalArgumentException("Medication Id cannot be null or less than zero");
-        }
-    }
 }
