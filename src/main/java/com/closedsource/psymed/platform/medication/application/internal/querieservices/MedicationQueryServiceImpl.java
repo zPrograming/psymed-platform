2655d24192c63b65487598e9592eb15aab74696f
RominaMaita <u202213765@upc.edu.pe>
2024-10-30 12:27:22 -0500

    feat(medication): add services to application tier.

--- src/main/java/com/closedsource/psymed/platform/medication/application/internal/querieservices/MedicationQueryServiceImpl.java
+++ src/main/java/com/closedsource/psymed/platform/medication/application/internal/querieservices/MedicationQueryServiceImpl.java
@@ -2,10 +2,8 @@ package com.closedsource.psymed.platform.medication.application.internal.queries
 
 import com.closedsource.psymed.platform.medication.domain.model.aggregates.Medication;
 import com.closedsource.psymed.platform.medication.domain.model.queries.GetAllMedicationsQuery;
-import com.closedsource.psymed.platform.medication.domain.model.queries.GetMedicationByIdQuery;
 import com.closedsource.psymed.platform.medication.domain.model.queries.GetMedicationByNameQuery;
 import com.closedsource.psymed.platform.medication.domain.services.MedicationQueryService;
-import com.closedsource.psymed.platform.medication.infrastructure.persistence.jpa.repositories.MedicationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,27 +11,11 @@ import java.util.Optional;
 
 @Service
 public class MedicationQueryServiceImpl implements MedicationQueryService {
-
-    private final MedicationRepository medicationRepository;
-
-    public MedicationQueryServiceImpl(MedicationRepository medicationRepository) {
-        this.medicationRepository = medicationRepository;
-    }
     @Override
     public Optional<Medication> handle(GetMedicationByNameQuery query) {
-
-        return medicationRepository.findByName(query.name());
     }
 
     @Override
     public List<Medication> handle(GetAllMedicationsQuery query) {
-
-        return medicationRepository.findAll();
-    }
-
-    @Override
-    public Optional<Medication> handle(GetMedicationByIdQuery query) {
-
-        return medicationRepository.findById(query.medicationId());
     }
 }
