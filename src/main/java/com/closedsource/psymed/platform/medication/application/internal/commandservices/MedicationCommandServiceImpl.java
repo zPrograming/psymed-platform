e249a68173df0e11c587f30a1c2e1a1589429be5
RominaMaita <u202213765@upc.edu.pe>
2024-10-30 12:27:22 -0500

    feat(medication): add services to application tier.

---
+++ src/main/java/com/closedsource/psymed/platform/medication/application/internal/commandservices/MedicationCommandServiceImpl.java
@@ -0,0 +1,19 @@
+package com.closedsource.psymed.platform.medication.application.internal.commandservices;
+
+import com.closedsource.psymed.platform.medication.domain.model.commands.CreateMedicationCommand;
+import com.closedsource.psymed.platform.medication.domain.model.commands.DeleteMedicationCommand;
+import com.closedsource.psymed.platform.medication.domain.services.MedicationCommandService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MedicationCommandServiceImpl implements MedicationCommandService {
+    @Override
+    public Long handle(CreateMedicationCommand command) {
+        return 0L;
+    }
+
+    @Override
+    public void handle(DeleteMedicationCommand command) {
+
+    }
+}
