5c6a8ff2370c1967f1164eb838b5b76e26da039e
RominaMaita <u202213765@upc.edu.pe>
2024-10-30 12:13:22 -0500

    feat: add queries.

--- src/main/java/com/closedsource/psymed/platform/medication/domain/model/queries/GetMedicationByNameQuery.java
+++ src/main/java/com/closedsource/psymed/platform/medication/domain/model/queries/GetMedicationByNameQuery.java
@@ -1,8 +1,4 @@
 package com.closedsource.psymed.platform.medication.domain.model.queries;
 
 public record GetMedicationByNameQuery(String name) {
-    public GetMedicationByNameQuery{
-        if(name == null || name.isBlank())
-            throw new IllegalArgumentException("Name cannot be null or empty");
-    }
 }
