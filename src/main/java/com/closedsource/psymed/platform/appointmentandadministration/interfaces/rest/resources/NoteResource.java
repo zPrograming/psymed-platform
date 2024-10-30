0013de441279c347cf32318bc61f45c97fbad070
David Bryan Rodriguez Santos <u202212236@upc.edu.pe>
2024-10-30 01:26:57 -0500

    feat(note): create note entities and services

--- src/main/java/com/closedsource/psymed/platform/appointmentandadministration/interfaces/rest/resources/NoteResource.java
+++
@@ -1,7 +0,0 @@
-package com.closedsource.psymed.platform.appointmentandadministration.interfaces.rest.resources;
-
-public record NoteResource(
-        Long id,
-        String description,
-        String symptom) {
-}
