61bb9acfe66acf3b115096efe5ca47a62e14a64c
RominaMaita <u202213765@upc.edu.pe>
2024-10-30 12:14:13 -0500

    feat(medication): add repository.

---
+++ src/main/java/com/closedsource/psymed/platform/medication/infrastructure/persistence/jpa/repositories/MedicationRepository.java
@@ -0,0 +1,11 @@
+package com.closedsource.psymed.platform.medication.infrastructure.persistence.jpa.repositories;
+
+import com.closedsource.psymed.platform.medication.domain.model.aggregates.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
+    Optional<Medication> findByName(String name);
+    boolean existsByName(String name);
+}
