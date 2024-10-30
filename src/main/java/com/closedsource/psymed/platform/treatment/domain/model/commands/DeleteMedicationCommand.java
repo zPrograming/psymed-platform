6a3c020c67d13bd2f5bc6ade421d3defb1d8b6b7
RominaMaita <u202213765@upc.edu.pe>
2024-10-30 12:10:32 -0500

    fix: remove unused classes.

--- src/main/java/com/closedsource/psymed/platform/treatment/domain/model/commands/DeleteMedicationCommand.java
+++
@@ -1,8 +0,0 @@
-package com.closedsource.psymed.platform.treatment.domain.model.commands;
-
-public record DeleteMedicationCommand(Long medicationId) {
-
-    public DeleteMedicationCommand{
-        if (medicationId ==null || medicationId <=0) throw new IllegalArgumentException("The medication Id cannot be 0 or less");
-    }
-}
