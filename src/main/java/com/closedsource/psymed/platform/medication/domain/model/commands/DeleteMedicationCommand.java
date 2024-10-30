c6cb521c1ab3f5ec16ec6533ea26fa77b43f1f0a
RominaMaita <u202213765@upc.edu.pe>
2024-10-30 12:55:24 -0500

    feat(medication): add command logic.

--- src/main/java/com/closedsource/psymed/platform/medication/domain/model/commands/DeleteMedicationCommand.java
+++ src/main/java/com/closedsource/psymed/platform/medication/domain/model/commands/DeleteMedicationCommand.java
@@ -1,4 +1,11 @@
 package com.closedsource.psymed.platform.medication.domain.model.commands;
 
-public record DeleteMedicationCommand() {
+public record DeleteMedicationCommand(Long medicationId) {
+
+    public DeleteMedicationCommand{
+        if( medicationId == null ||medicationId <= 0)
+        {
+            throw new IllegalArgumentException("Medication Id cannot be null or less than zero");
+        }
+    }
 }
