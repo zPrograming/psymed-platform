ecfb0b2251bde0d778242cfe73f8c4c27a114a72
David Bryan Rodriguez Santos <u202212236@upc.edu.pe>
2024-10-30 01:26:57 -0500

    feat(note): create note entities and services

--- src/main/java/com/closedsource/psymed/platform/appointmentandadministration/infrastructure/persistence/jpa/repositories/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.closedsource.psymed.platform.appointmentandadministration.infrastructure.persistence.jpa.repositories;
-
-import com.closedsource.psymed.platform.appointmentandadministration.domain.model.entities.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
