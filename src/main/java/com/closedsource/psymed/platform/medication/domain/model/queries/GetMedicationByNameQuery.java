e670033afbf503f19d4e39e7918b436ae5542358
RominaMaita <u202213765@upc.edu.pe>
2024-10-30 20:37:33 -0500

    feat(medication): add business rules for commands and queries.

--- src/main/java/com/closedsource/psymed/platform/medication/domain/model/queries/GetMedicationByNameQuery.java
+++ src/main/java/com/closedsource/psymed/platform/medication/domain/model/queries/GetMedicationByNameQuery.java
@@ -1,4 +1,8 @@
 package com.closedsource.psymed.platform.medication.domain.model.queries;
 
 public record GetMedicationByNameQuery(String name) {
+    public GetMedicationByNameQuery{
+        if(name == null || name.isBlank())
+            throw new IllegalArgumentException("Name cannot be null or empty");
+    }
 }
