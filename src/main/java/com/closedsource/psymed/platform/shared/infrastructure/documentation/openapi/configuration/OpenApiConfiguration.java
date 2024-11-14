a4a6a8bbdf22e4750c439e14066c0795151e88bb
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-11-14 13:22:07 -0500

    feat(shared): open api configuration added

---
+++ src/main/java/com/closedsource/psymed/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,49 @@
+package com.closedsource.psymed.platform.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Value("${documentation.application.description}")
+    String applicationDescription;
+
+    @Value("${documentation.application.version}")
+    String applicationVersion;
+
+    @Bean
+    public OpenAPI learningPlatformOpenApi() {
+        var openApi = new OpenAPI();
+        openApi.info(new Info()
+                        .title(applicationName)
+                        .description(applicationDescription)
+                        .version(applicationVersion)
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Psymed wiki Documentation")
+                        .url("https://psymed.wiki.github.io/docs"));
+        // Add security definitions
+        String securitySchemeName = "bearerAuth";
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+        // Return the OpenAPI object with the configuration
+        return openApi;
+    }
+}
